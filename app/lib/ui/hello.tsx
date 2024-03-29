b1649bcd9284f54d8ba5c76a77d65be5afca251a
clodi <ciao@clodi.tech>
2024-03-29 12:10:41 +0100

    hello

--- app/lib/ui/hello.tsx
+++ app/lib/ui/hello.tsx
@@ -2,7 +2,15 @@ import Image from 'next/image';
 
 export default function Hello() {
     return (
-        <Image src="/about.jpeg" alt="about" className="rounded-full"
-          width={100} height={100}/>
+        <>
+            <div className='flex flex-col gap-4 max-w-sm'>
+                <div className='flex justify-center items-center gap-4'>
+                    <Image src="/about.jpeg" alt="about" className="rounded-3xl border-2 border-gray-600"
+                        width={100} height={100}/>
+                    <h1>hey! it's me, clod.</h1>
+                </div>
+                <p className='text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </>
     );
 }
