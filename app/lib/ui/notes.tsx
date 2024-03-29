9817d35652d378f33dd1104d1a72428ba3c747b8
clodi <ciao@clodi.tech>
2024-03-29 16:46:17 +0100

    default heading

--- app/lib/ui/notes.tsx
+++ app/lib/ui/notes.tsx
@@ -1,23 +1,24 @@
 import { Accordion, AccordionItem } from "@nextui-org/react";
 
+const defaultHeading = "the idea behind building a system";
 const defaultContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 const notes = [
     {
         title: 'third note',
         date: '2024.03.01',
-        heading: 'the idea behind building a system',
+        heading: defaultHeading,
         content: defaultContent
     },
     {
         title: 'second note',
         date: '2024.02.01',
-        heading: 'the idea behind building a system',
+        heading: defaultHeading,
         content: defaultContent
     },
     {
         title: 'first note',
         date: '2024.01.01',
-        heading: 'the idea behind building a system',
+        heading: defaultHeading,
         content: defaultContent
     },
 ]
