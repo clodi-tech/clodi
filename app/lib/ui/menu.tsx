c41f8110f13c6a136fd479659c1e401552fa4cec
clodi <ciao@clodi.tech>
2024-04-08 11:56:48 +0200

    use path

--- app/lib/ui/menu.tsx
+++ app/lib/ui/menu.tsx
@@ -1,12 +1,16 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Menu() {
+  const pathname = usePathname();
 
   return (
-    <main>
-      <Link href="/">hello</Link>
-      <Link href="/systems">systems</Link>
-      <Link href="/notes">notes</Link>
-    </main>
+    <div className="flex justify-center gap-4">
+      <Link href="/" className={pathname === '/' ? 'text-white underline underline-offset-4' : ''}>hello</Link>
+      <Link href="/systems" className={pathname === '/systems' ? 'text-white' : ''}>systems</Link>
+      <Link href="/notes" className={pathname === '/notes' ? 'text-white' : ''}>notes</Link>
+    </div>
   );
 }
