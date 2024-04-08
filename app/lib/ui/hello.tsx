5ce2e7181511e58b6c7493756d8095af03dad558
clodi <ciao@clodi.tech>
2024-04-08 11:07:29 +0200

    hello copy

--- app/lib/ui/hello.tsx
+++ app/lib/ui/hello.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image';
 
+const title = "hey! it's me, clod.";
+const bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
 export default function Hello() {
     return (
         <>
@@ -7,9 +10,9 @@ export default function Hello() {
                 <div className='flex justify-center items-center gap-4'>
                     <Image src="/about.jpeg" alt="about" className="rounded-3xl border-2 border-gray-600"
                         width={100} height={100}/>
-                    <h1>hey! it&apos;s me, clod.</h1>
+                    <h1>{title}</h1>
                 </div>
-                <p className='text-justify'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className='text-justify'>{bio}</p>
             </div>
         </>
     );
