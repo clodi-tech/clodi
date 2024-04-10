6af406ea3e999d8495a1ccbc0431248faf86584b
clodi <ciao@clodi.tech>
2024-04-10 13:34:45 +0200

    fix warning key

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,5 +1,6 @@
 import { Divider } from "@nextui-org/react";
 import Link from "next/link";
+import { Fragment } from "react";
 
 const title = "a collection of notes to lighten the mind.";
 const description = "";
@@ -33,13 +34,13 @@ export default function Page() {
             </div>
             <div className="flex flex-col gap-2 w-full max-w-sm">
                 {notes.map((note, index) => (
-                    <>
-                        <Divider key={index} />
-                        <Link key={index} href={`/notes/${note.slug}`} className="px-3 py-1 rounded-lg hover:bg-gray-800 transition-colors duration-500">
+                    <Fragment key={index}>
+                        <Divider />
+                        <Link href={`/notes/${note.slug}`} className="px-3 py-1 rounded-lg hover:bg-gray-800 transition-colors duration-500">
                             <h3>{note.title}</h3>
                             <small>{note.date}</small>
                         </Link>
-                    </>
+                    </Fragment>
                 ))}
             </div>
         </main>
