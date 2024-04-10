97d67c183183413c78adf407a53809796285aa16
clodi <ciao@clodi.tech>
2024-04-10 12:12:04 +0200

    tuning systems

--- app/systems/page.tsx
+++ app/systems/page.tsx
@@ -1,7 +1,7 @@
 import { Card, CardBody, CardFooter, CardHeader, Chip } from '@nextui-org/react';
 import Image from 'next/image';
 
-const title = "a collection of systems to engineer freedom.";
+const title = "curated systems to engineer freedom.";
 const description = "";
 const defaultContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 const systems = [
@@ -18,16 +18,16 @@ const systems = [
         logo: '/sorival.svg'
     },
     {
-        name: 'lightime',
-        description: defaultContent,
-        link: 'https://lightime.vercel.app/',
-        logo: '/lightime.svg'
-    },
-    {
         name: 'goldenr',
         description: defaultContent,
         link: 'https://goldenr.vercel.app/',
         logo: '/goldenr.svg'
+    },
+    {
+        name: 'lightime',
+        description: defaultContent,
+        link: 'https://lightime.vercel.app/',
+        logo: '/lightime.svg'
     },
     {
         name: 'dgames',
@@ -52,28 +52,26 @@ const systems = [
 export default function Page() {
     return (
         <main>
-            <div className='flex flex-col justify-center items-center gap-4'>
-                <div className="text-center max-w-md">
-                    <h2>{title}</h2>
-                    <p>{description}</p>
-                </div>
-                {systems.map((system, index) => (
-                    <Card key={index} className="max-w-sm">
-                        <CardHeader className="flex item-center gap-4 pb-1">
-                            <Image src={system.logo} alt={system.name} width={40} height={40} />
-                            <h3>{system.name}</h3>
-                        </CardHeader>
-                        <CardBody className='py-0'>
-                            <p className='text-justify'>{system.description}</p>
-                        </CardBody>
-                        <CardFooter>
-                            <a href={system.link} target='_blank' rel='noopener noreferrer'>
-                                <Chip variant='faded' color='primary'>launch app</Chip>
-                            </a>
-                        </CardFooter>
-                    </Card>
-                ))}
+            <div className="text-center max-w-md">
+                <h2>{title}</h2>
+                <p>{description}</p>
             </div>
+            {systems.map((system, index) => (
+                <Card key={index} className="max-w-sm">
+                    <CardHeader className="flex item-center gap-4 pb-1">
+                        <Image src={system.logo} alt={system.name} width={40} height={40} />
+                        <h3>{system.name}</h3>
+                    </CardHeader>
+                    <CardBody className='py-0'>
+                        <p className='text-justify'>{system.description}</p>
+                    </CardBody>
+                    <CardFooter>
+                        <a href={system.link} target='_blank' rel='noopener noreferrer'>
+                            <Chip variant='faded' color='primary'>launch app</Chip>
+                        </a>
+                    </CardFooter>
+                </Card>
+            ))}
         </main>
     );
 }
