8e6db0be1f20dacb244c02c3f1eacb475ab1ed15
clodi <ciao@clodi.tech>
2024-04-11 13:08:55 +0200

    custom cards

--- app/systems/page.tsx
+++ app/systems/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, CardFooter, CardHeader, Chip } from '@nextui-org/react';
+import { Chip } from '@nextui-org/react';
 import Image from 'next/image';
 import { systemsData } from './systems';
 
@@ -13,20 +13,20 @@ export default function Page() {
                 <p>{description}</p>
             </div>
             {systemsData.map((system, index) => (
-                <Card key={index} className="max-w-sm">
-                    <CardHeader className="flex item-center gap-4 pb-1">
+                <div key={index} className="max-w-sm p-4 rounded-xl border border-gray-700 shadow hover:border-gray-400 transition-colors duration-500">
+                    <div className="flex items-center gap-3">
                         <Image src={system.logo} alt={system.name} width={40} height={40} />
                         <h3>{system.name}</h3>
-                    </CardHeader>
-                    <CardBody className='py-0'>
+                    </div>
+                    <div className='py-3'>
                         <p className='text-justify'>{system.description}</p>
-                    </CardBody>
-                    <CardFooter>
+                    </div>
+                    <div className='flex items-center gap-2'>
                         <a href={system.link} target='_blank' rel='noopener noreferrer'>
                             <Chip variant='faded' color='primary'>launch app</Chip>
                         </a>
-                    </CardFooter>
-                </Card>
+                    </div>
+                </div>
             ))}
         </main>
     );
