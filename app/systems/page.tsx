c40b107c5832d0afd9e1273439fccd3745946137
clodi <ciao@clodi.tech>
2024-04-11 17:19:36 +0200

    hover color remove referrer

--- app/systems/page.tsx
+++ app/systems/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
                 <p>{description}</p>
             </div>
             {systemsData.map((system, index) => (
-                <div key={index} className="max-w-sm p-4 rounded-xl border border-gray-700 shadow hover:border-gray-400 transition-colors duration-500">
+                <div key={index} className="max-w-sm p-4 rounded-xl border border-gray-700 shadow hover:border-gray-100 transition-colors duration-500">
                     <div className="flex items-center gap-3">
                         <Image src={system.logo} alt={system.name} width={40} height={40} />
                         <h3>{system.name}</h3>
@@ -22,7 +22,7 @@ export default function Page() {
                         <p className='text-justify'>{system.description}</p>
                     </div>
                     <div className='flex items-center gap-2'>
-                        <a href={system.link} target='_blank' rel='noopener noreferrer'>
+                        <a href={system.link} target='_blank' rel='noopener'>
                             <Chip variant='faded' color='primary'>launch app</Chip>
                         </a>
                     </div>
