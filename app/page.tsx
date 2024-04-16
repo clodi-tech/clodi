47aa78cb9ddb8216b27cec70fce9f72a199f1d53
clodi <ciao@clodi.tech>
2024-04-16 11:00:09 +0200

    copy home

--- app/page.tsx
+++ app/page.tsx
@@ -16,10 +16,10 @@ export default function Home() {
                         <small className='text-gray-500'>{small}</small>
                     </div>
                 </div>
-                <p className='text-justify'>I am a developer who finds joy in the <b>art of coding</b> at the intersection between humans, nature and technology, where elusive connections can converge into something magical.</p>
-                <p className='text-justify'>With nearly ninety months in the corporate world, delivering digital solutions that touched thousands of lives, i realised that was not Enough. I did not want to just contribute but to <b>innovate</b>. I want to create solutions to everyday challenges that resonate with humans, just like you and me.</p>
-                <p className='text-justify'>I want to find that <b>Enough</b>, as i am here to help you find yours - a point in time and space where calm, clarity and joy are the only constants. I know it is somewhere within ourselves and technology may help us find it.</p>
-                <p className='text-justify'>No matter how far or close you are from your enough, your insights could spark the next magical system we may build together.<br />... Feel free to reach me out. Here and now.</p>
+                <p className='text-justify'>I am a developer who finds joy in the <b>art of coding</b> at the intersection between <u>humans</u>, <u>nature</u> and <u>technology</u>, where elusive connections can converge into something magical.</p>
+                <p className='text-justify'>After delivering, on behalf of others, diverse digital solutions that touched thousands of lives, I realised <u>that was not Enough</u> for me. I did not want to just contribute but to <b>create solutions</b> for everyday challenges that resonate with humans, just like you and me.</p>
+                <p className='text-justify'>I want to find that <b>Enough</b> - a point in time and space where abundance meets us and <u>we have everything we need</u>. I know it is somewhere within ourselves and a mindful use of technology may help us find it.</p>
+                <p className='text-justify'>I am here to craft <b>systems</b> you can use. I am here to write <b>notes</b> you can read. I am here to help us find our Enough.</p>
             </div>
             <a href="/resume.pdf" target='_blank' rel='noopener'>
                 <div className='flex justify-center items-center gap-1'>
