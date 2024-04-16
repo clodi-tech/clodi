54da0d92da599b7ca5d84f96f7bfc57d746695a4
clodi <ciao@clodi.tech>
2024-04-16 14:17:37 +0200

    size resume

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <footer className='p-4 gap-2 flex flex-col justify-center items-center'>
       <a href="/resume.pdf" target='_blank' rel='noopener'>
-          <div className='flex justify-center items-center gap-1'>
-              <Image src="/download.svg" alt='download' width={30} height={30} />
+          <div className='flex justify-center items-center gap-2'>
+              <Image src="/download.svg" alt='download' width={15} height={15} />
               <p className='text-white'>resume</p>
           </div>
       </a>
