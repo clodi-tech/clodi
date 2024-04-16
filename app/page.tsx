bcb9f1381a6b7880a45b9e72c9cf4b7678b99fdb
clodi <ciao@clodi.tech>
2024-04-16 12:16:28 +0200

    cta

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 const title = "Hey! It's me, Clodi.";
 const small = "Homo Sapiens - Planet Earth - Internet";
 const size = 100;
+const FORM = "https://clodi.ck.page/7a1db31a8b";
 
 export default function Home() {
     return (
@@ -17,16 +18,11 @@ export default function Home() {
                     </div>
                 </div>
                 <p className='text-justify'>I am a developer who finds joy in the <b>art of coding</b> at the intersection between <u>humans</u>, <u>nature</u> and <u>technology</u> - where elusive connections can converge into something magical.</p>
-                <p className='text-justify'>In my carreer, I delivered digital solutions that touched thousands of lives - yet after years in the field I realised <u>that was not Enough</u> for me. All I want is to <b>create solutions</b> for everyday challenges that resonate with humans, just like you and me.</p>
-                <p className='text-justify'>Today, my journey is about finding that <b>Enough</b> - a point in time and space where abundance meets us and <u>we have everything we need</u>. I know it is somewhere within ourselves and a mindful use of technology may guide us there.</p>
-                <p className='text-justify'>I am here to craft <b>systems</b> you can use, write <b>notes</b> you can read, and help us find our Enough.</p>
+                <p className='text-justify'>In my career, I delivered digital solutions that touched thousands of lives - yet after years in the field I realised <u>that was not Enough</u> for me. All I want is to <b>create solutions</b> for everyday challenges that resonate with humans, just like you and me.</p>
+                <p className='text-justify'>Today, my journey is about finding that <b>Enough</b> - a point in time and space where abundance meets us and <u>we have everything we need</u>. I feel it is somewhere within ourselves and a mindful use of technology may guide us there.</p>
+                <p className='text-justify'>I am here to craft <b>systems</b> you can use, write <b>notes</b> you can read. And if you start questioning what is Enough for you, subscribe for free and start your journey with five quests about Enough.</p>
+                <a href={FORM} target='_blank' rel='noopener'><small className='text-white'><b>Here and now.</b></small></a>
             </div>
-            <a href="/resume.pdf" target='_blank' rel='noopener'>
-                <div className='flex justify-center items-center gap-1'>
-                    <Image src="/download.svg" alt='download' width={30} height={30} />
-                    <p className='text-white'>resume</p>
-                </div>
-            </a>
         </main>
     );
 }
--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,6 +1,14 @@
+import Image from 'next/image';
+
 export default function Footer() {
   return (
     <footer className='p-4 gap-2 flex flex-col justify-center items-center'>
+      <a href="/resume.pdf" target='_blank' rel='noopener'>
+          <div className='flex justify-center items-center gap-1'>
+              <Image src="/download.svg" alt='download' width={30} height={30} />
+              <p className='text-white'>resume</p>
+          </div>
+      </a>
       <p className='cursor-help'>© 2092</p>
     </footer>
   );
