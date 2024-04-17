f552dbe1beae350547443a3f7b686dc5767fa825
clodi <ciao@clodi.tech>
2024-04-17 10:51:13 +0200

    notes to db

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,13 +1,14 @@
 import { Divider } from "@nextui-org/react";
 import Link from "next/link";
 import { Fragment } from "react";
-import { notesData } from "./notes";
+import { sql } from "@vercel/postgres";
 
 const title = "Curated notes to define Enough.";
 const description = "Dive into the ideas, questions and pioneers that inspired our systems. These notes are the foundation for meaningful human-machine interactions on our journey towards Enough.";
 const cta = "Pick one and read it. Here and now.";
 
-export default function Page() {
+export default async function Page() {
+    const { rows } = await sql`SELECT * from NOTES`;
 
     return (
         <main>
@@ -17,7 +18,7 @@ export default function Page() {
                 <small className='text-gray-500'>{cta}</small>
             </div>
             <div className="flex flex-col gap-2 w-full max-w-sm">
-                {notesData.map((note, index) => (
+                {rows.map((note, index) => (
                     <Fragment key={index}>
                         <Divider />
                         <Link href={`/notes/${note.slug}`} className="px-3 py-1 rounded-lg hover:bg-gray-800 transition-colors duration-500">
