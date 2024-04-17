4fe99b1067b3ae2fa27611f2309945829bf66044
clodi <ciao@clodi.tech>
2024-04-17 11:03:19 +0200

    note page

--- app/notes/[slug]/page.tsx
+++ app/notes/[slug]/page.tsx
@@ -1,31 +1,23 @@
-import { notesData } from "../notes";
 import Image from 'next/image';
+import { sql } from "@vercel/postgres";
 
-export default function Page({ params }: { params: { slug: string } }) {
-    const note = notesData.find((note) => note.slug === params.slug);
-
-    if(!note) {
-        return <main>404 - note not found</main>;
-    }
+export default async function Page({ params }: { params: { slug: string } }) {
+    const { rows } = await sql`SELECT * from CHAPTERS where note_slug=${params.slug}`;
 
     return (
         <main>
-            <div className="text-center max-w-sm">
-                <h3>{note.title}</h3>
-                <small className='text-gray-500'>{note.date}</small>
+        {rows.map((chapter, index) => (
+            <div key={index} className="text-justify w-full max-w-sm">
+                <h2>{chapter.title}</h2>
+                <p>{chapter.content}</p>
+                {chapter.image ?
+                    <div className="flex justify-center items-center m-2">
+                        <Image src={chapter.image} alt="image" 
+                            width={chapter.width} height={chapter.height}/>
+                    </div>
+                    : null}
             </div>
-            {note.content.map((chapter, index) => (
-                <div key={index} className="text-justify w-full max-w-sm">
-                    <h2>{chapter.title}</h2>
-                    <p>{chapter.content}</p>
-                    {chapter.image ?
-                        <div className="flex justify-center items-center m-2">
-                            <Image src={chapter.image} alt="image" 
-                                width={chapter.width} height={chapter.height}/>
-                        </div>
-                        : null}
-                </div>
-            ))}
+        ))}
         </main>
     );
 }
