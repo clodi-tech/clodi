2de88a7f4e001f8e43ba4d026e44ee8346a28767
clodi <ciao@clodi.tech>
2024-04-17 12:20:17 +0200

    app link add github

--- app/systems/page.tsx
+++ app/systems/page.tsx
@@ -5,6 +5,7 @@ import { sql } from "@vercel/postgres";
 const title = "Curated systems to achieve Enough.";
 const description = "Inspired by the wisdom of great minds, these systems are designed to get us closer to Enough and enrich our journey on Planet Earth.";
 const cta = "Engage with them. Here and now.";
+const size = 40;
 
 export default async function Page() {
     const { rows: systems } = await sql`SELECT * from SYSTEMS`;
@@ -16,18 +17,23 @@ export default async function Page() {
                 <small className='text-gray-500'>{cta}</small>
             </div>
             {systems.map((system, index) => (
+                console.log(system),
                 <div key={index} className="max-w-sm p-4 rounded-xl border border-gray-700 shadow hover:border-gray-100 transition-colors duration-500">
                     <div className="flex items-center gap-3">
-                        <Image src={system.logo} alt={system.name} width={40} height={40} />
+                        <Image src={system.logo} alt={system.name} width={size} height={size} />
                         <h3>{system.name}</h3>
                     </div>
                     <div className='py-3'>
                         <p className='text-justify'>{system.description}</p>
                     </div>
                     <div className='flex items-center gap-2'>
-                        <a href={system.link} target='_blank' rel='noopener'>
+                        <a href={system.app} target='_blank' rel='noopener'>
                             <Chip variant='faded' color='primary'>launch app</Chip>
                         </a>
+                        {system.github ? 
+                        <a href={system.github} target='_blank' rel='noopener'>
+                            <Chip variant='faded' color='secondary'>github</Chip>
+                        </a> : null}
                     </div>
                 </div>
             ))}
