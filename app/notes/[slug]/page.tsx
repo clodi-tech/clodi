cd593778357d19fec543df9c40b04d7aa47a47e2
clodi <ciao@clodi.tech>
2024-04-22 14:32:51 +0200

    notes page comment

--- app/notes/[slug]/page.tsx
+++ app/notes/[slug]/page.tsx
@@ -3,14 +3,20 @@ import { sql } from "@vercel/postgres";
 import { revalidatePath } from 'next/cache';
 
 export default async function Page({ params }: { params: { slug: string } }) {
+    // incrementing the views counter
     await sql`UPDATE NOTES SET views = views + 1 where slug=${params.slug}`;
+
+    // revalidating the cache
     revalidatePath(`/notes/${params.slug}`);
     revalidatePath("/notes");
+
+    // fetching the note and its chapters from the database
     const { rows: [note] } = await sql`SELECT * from NOTES where slug=${params.slug}`;
     const { rows: chapters } = await sql`SELECT * from CHAPTERS where note_slug=${params.slug}`;
 
     return (
         <main>
+            {/* note details */}
             <div className="text-center max-w-sm">
                 <h3>{note.title}</h3>
                 <small className='text-gray-500'>{note.date}</small>
@@ -19,8 +25,12 @@ export default async function Page({ params }: { params: { slug: string } }) {
                     <Image src="/views.svg" alt="views" width={15} height={15}/>
                 </div>
             </div>
+
+            {/* note chapters */}
             {chapters.map((chapter, index) => (
                 <div key={index} className="text-justify w-full max-w-sm">
+
+                    {/* chapter title and content, oprtional image */}
                     <h2>{chapter.title}</h2>
                     <p>{chapter.content}</p>
                     {chapter.image ?
@@ -31,6 +41,8 @@ export default async function Page({ params }: { params: { slug: string } }) {
                         : null}
                 </div>
             ))}
+
+            {/* call to action */}
             {note.cta ? 
             <div className="text-justify w-full max-w-sm">
                 <p>{note.cta}</p>
