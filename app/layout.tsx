1069212c4248c8209a2acb730db73d084f1b3c73
clodi <ciao@clodi.tech>
2024-04-23 22:37:25 +0200

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,23 @@
-import type { Metadata } from 'next';
-import { Rajdhani } from 'next/font/google';
-import { Providers } from './providers';
-import './globals.css';
+import type { Metadata } from "next";
+import { Rajdhani } from "next/font/google";
+import { Providers } from "./providers";
+import "./globals.css";
 
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import Header from "@/app/ui/header";
 import Menu from "@/app/ui/menu";
 import Footer from "@/app/ui/footer";
 
-const font = Rajdhani({ 
+const font = Rajdhani({
   subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"]
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
-  title: 'clodi',
-  description: 'operating systems for engineering freedom.',
+  title: "clodi",
+  description: "operating systems for engineering freedom.",
 };
 
 export default function RootLayout({
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' className='dark'>
+    <html lang="en" className="dark">
       <body className={font.className}>
         <Providers>
           <Header />
