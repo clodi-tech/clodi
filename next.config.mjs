20dfb2c6e8e1b8a36b6d6d2f07d3fcdea4f09c4f
clodi <ciao@clodi.tech>
2024-04-25 11:25:38 +0200

    image remote pattern

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'via.placeholder.com',
-            port: '',
-            pathname: '/150',
-          },
-        ],
-      },  
-};
+const nextConfig = {};
 
 export default nextConfig;
