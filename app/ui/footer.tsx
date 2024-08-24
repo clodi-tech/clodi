a1feaa808b2c7bf007409bed4e2f48d3c3c35006
clodi <ciao@clodi.tech>
2024-08-24 15:00:47 +0200

    icons

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,14 +1,31 @@
 import Image from "next/image";
 
+const icons = [
+  { src: "/twitter.svg", alt: "twitter", href: "https://x.com/clodi_tech" },
+  { src: "/github.svg", alt: "github", href: "https://github.com/clodi-tech" },
+  {
+    src: "/linkedin.svg",
+    alt: "linkedin",
+    href: "https://www.linkedin.com/in/claudio-calle/",
+  },
+];
+
+function Icon({ src, alt, href }: { src: string; alt: string; href: string }) {
+  return (
+    <a href={href} target="_blank" rel="noopener">
+      <Image src={src} alt={alt} width={15} height={15} />
+    </a>
+  );
+}
+
 export default function Footer() {
   return (
-    <footer className="p-4 gap-2 flex flex-col justify-center items-center">
-      <a href="/resume.pdf" target="_blank" rel="noopener">
-        <div className="flex justify-center items-center gap-2">
-          <Image src="/download.svg" alt="download" width={15} height={15} />
-          <p className="text-white">resume</p>
-        </div>
-      </a>
+    <footer className="p-4 gap-4 flex flex-col justify-center items-center">
+      <div className="flex gap-4">
+        {icons.map((icon, index) => (
+          <Icon key={index} {...icon} />
+        ))}
+      </div>
       <p className="cursor-help">© 2092</p>
     </footer>
   );
