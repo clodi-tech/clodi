314152db52f4cf5d04d3b221e261969b7903e8bb
clodi <ciao@clodi.tech>
2025-05-07 12:37:05 +0200

    come back soon

---
+++ lib/const.ts
@@ -0,0 +1,11 @@
+export const Links = {
+  linkedin: "https://www.linkedin.com/in/claudio-calle/",
+};
+
+import { Rajdhani as FontSans } from "next/font/google";
+
+export const fontSans = FontSans({
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
+  subsets: ["latin"],
+});
