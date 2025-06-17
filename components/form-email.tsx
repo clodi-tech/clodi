2a9c739cbf098afc979ba5214350a98f2608eada
clodi <ciao@clodi.tech>
2025-06-17 16:08:40 +0200

    handle state

--- components/form-email.tsx
+++ components/form-email.tsx
@@ -10,16 +10,22 @@ export default function FormEmail() {
   const [state, formAction] = useActionState(getEmail, null);
 
   return (
-    <Form action={formAction} className="flex gap-2 w-full max-w-xs">
-      <Input
-        type="email"
-        placeholder="Your email"
-        name="email"
-        required
-        className="flex-1"
-      />
-      <Button type="submit">Send</Button>
-      {state && state === "ok" && <p>Email sent</p>}
-    </Form>
+    <div className="flex flex-col items-center gap-2 w-full">
+      <Form action={formAction} className="flex gap-2 w-full max-w-xs">
+        <Input
+          type="email"
+          placeholder="Your email"
+          name="email"
+          required
+          className="flex-1"
+        />
+        <Button type="submit">Send</Button>
+      </Form>
+      {state && state === "ok" && (
+        <small className="text-green-500">
+          Thanks! I will get back to you soon.
+        </small>
+      )}
+    </div>
   );
 }
