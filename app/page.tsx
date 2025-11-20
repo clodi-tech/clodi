31e9a2b5dcb51ebd193d0ff06c7bc49e99c35917
clodi <ciao@clodi.tech>
2025-11-20 10:29:16 +0100

    setup copy and structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,38 @@
 export default function Home() {
   return (
-    <main>
-      <div className="font-sans">Clodi</div>
-      <div className="font-mono">Clodi</div>
+    <main className="flex flex-col items-center min-h-screen p-10 gap-10">
+      <div className="flex flex-col items-center gap-1">
+        <div>logo</div>
+        <div className="flex gap-2">
+          <div>image</div>
+          <div>HEYCIAO</div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center gap-1">
+        <div>SONOCLODI</div>
+        <div>creo progetti tech su misura, con cura.</div>
+      </div>
+      <div className="flex flex-col items-center gap-2">
+        <div>MIEIPROGETTI</div>
+        <div className="flex flex-col items-center">
+          <div>progetto 1</div>
+          <div>ti racconto il progetto 1</div>
+        </div>
+        <div className="flex flex-col items-center">
+          <div>progetto 2</div>
+          <div>ti racconto il progetto 2</div>
+        </div>
+        <div className="flex flex-col items-center">
+          <div>progetto 3</div>
+          <div>ti racconto il progetto 3</div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center gap-1">
+        <div>icona</div>
+        <div>il prossimo potrebbe essere il tuo</div>
+        <div>input email</div>
+        <div>button</div>
+      </div>
     </main>
   );
 }
