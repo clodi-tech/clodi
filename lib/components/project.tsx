8f19f2582164a9e9ae2df7b184ed5ddab39f7b20
clodi <ciao@clodi.tech>
2025-11-20 17:53:57 +0100

    layout projects

--- lib/components/project.tsx
+++ lib/components/project.tsx
@@ -2,7 +2,7 @@ import type { Project } from "@/lib/types";
 
 export default function Project({ title, description }: Project) {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col w-full">
       <h3 className="uppercase text-light font-bold">{title}</h3>
       <p className="text-sm text-mid">{description}</p>
     </div>
